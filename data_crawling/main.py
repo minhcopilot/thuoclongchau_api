6a3c5920139061f96d4469b755f58b298ec6111d
minh phan <21115053120333@sv.ute.udn.vn>
2024-04-27 12:37:18 +0700

    add data_crawling api and init.sql for data_crawling

---
+++ data_crawling/main.py
@@ -0,0 +1,115 @@
+from fastapi import FastAPI
+import requests
+import mysql.connector
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+DATABASE_NAME=os.getenv("DB_NAME")
+HOST=os.getenv("DB_HOST")
+PORT=os.getenv("DB_PORT")
+USER=os.getenv("DB_USER")
+PASSWORD=os.getenv("DB_PASSWORD")
+# Kết nối đến cơ sở dữ liệu MySQL
+db = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE_NAME,
+    port=PORT
+)
+cursor = db.cursor()
+
+app = FastAPI()
+current_skip_count = 0
+
+@app.get("/")
+def index():
+    return {"title": "Hello:)"}
+@app.post("/save_products")
+async def save_products():
+    global current_skip_count
+    max_result_count = 5
+    url = "https://api.nhathuoclongchau.com.vn/lccus/search-product-service/api/products/ecom/product/search/cate"
+    body = {"skipCount": current_skip_count, "maxResultCount": max_result_count}
+    response = requests.post(url, json=body)
+
+    # Kiểm tra nếu yêu cầu thành công
+    if response.status_code == 200:
+        data = response.json()
+
+        # Xử lý dữ liệu và lưu vào cơ sở dữ liệu
+        for product in data["products"]:
+            # Lưu thông tin sản phẩm vào bảng products
+            insert_product_query = """
+                INSERT INTO products (sku, name, webName, image, slug, ingredients, dosageForm, brand, displayCode, isActive, isPublish, searchScoring, productRanking, specification)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON DUPLICATE KEY UPDATE
+            name = VALUES(name),
+            webName = VALUES(webName),
+            image = VALUES(image),
+            slug = VALUES(slug),
+            ingredients = VALUES(ingredients),
+            dosageForm = VALUES(dosageForm),
+            brand = VALUES(brand),
+            displayCode = VALUES(displayCode),
+            isActive = VALUES(isActive),
+            isPublish = VALUES(isPublish),
+            searchScoring = VALUES(searchScoring),
+            productRanking = VALUES(productRanking),
+            specification = VALUES(specification)
+            """
+            product_values = (
+                product["sku"], product["name"], product["webName"], product["image"], product["slug"], product["ingredients"],
+                product["dosageForm"], product["brand"], product["displayCode"], product["isActive"], product["isPublish"],
+                product["searchScoring"], product["productRanking"], product["specification"]
+            )
+            cursor.execute(insert_product_query, product_values)
+
+            # Lưu thông tin giá của sản phẩm vào bảng prices
+            for price in product["prices"]:
+                insert_price_query = """
+                    INSERT INTO prices (id,productSKU, measureUnitCode, measureUnitName, isSellDefault, price, currencySymbol, isDefault, inventory, isInventory, level)
+                    VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
+                measureUnitCode = VALUES(measureUnitCode),
+                measureUnitName = VALUES(measureUnitName),
+                isSellDefault = VALUES(isSellDefault),
+                price = VALUES(price),
+                currencySymbol = VALUES(currencySymbol),
+                isDefault = VALUES(isDefault),
+                inventory = VALUES(inventory),
+                isInventory = VALUES(isInventory),
+                level = VALUES(level)
+                """
+                price_values = (
+                    price["id"],product["sku"], price["measureUnitCode"], price["measureUnitName"], price["isSellDefault"], price["price"],
+                    price["currencySymbol"], price["isDefault"], price["inventory"], price["isInventory"], price["level"]
+                )
+                cursor.execute(insert_price_query, price_values)
+            
+            # Lưu thông tin danh mục của sản phẩm vào bảng categories
+            for category in product["category"]:
+                insert_category_query = """
+                INSERT INTO categories (id, name, parentName, slug, level, isActive, productSKU)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ON DUPLICATE KEY UPDATE name=VALUES(name), parentName=VALUES(parentName), slug=VALUES(slug), level=VALUES(level), isActive=VALUES(isActive);
+                """
+                category_values = (
+                    category["id"], category["name"], category["parentName"], category["slug"], category["level"],
+                    category["isActive"], product["sku"]
+                )
+                cursor.execute(insert_category_query, category_values)
+
+        # Commit các thay đổi vào cơ sở dữ liệu
+        db.commit()
+        current_skip_count += max_result_count
+        return {"message": "Dữ liệu đã được lưu thành công vào cơ sở dữ liệu"}
+
+    # Trả về thông báo lỗi nếu yêu cầu không thành công
+    else:
+        return {"error": f"Yêu cầu không thành công. Mã lỗi: {response.status_code}"}
---
+++ mysql/init_db/init.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE thuoclongchau;
+USE thuoclongchau;
+
+CREATE TABLE products (
+    sku VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    webName VARCHAR(255),
+    image VARCHAR(255),
+    slug VARCHAR(255),
+    ingredients TEXT,
+    dosageForm VARCHAR(255),
+    brand VARCHAR(255),
+    displayCode INT,
+    isActive BOOLEAN,
+    isPublish BOOLEAN,
+    searchScoring FLOAT,
+    productRanking INT,
+    specification VARCHAR(255)
+);
+
+CREATE TABLE prices (
+    id INT PRIMARY KEY,
+    productSKU VARCHAR(255),
+    measureUnitCode INT,
+    measureUnitName VARCHAR(255),
+    isSellDefault BOOLEAN,
+    price FLOAT,
+    currencySymbol VARCHAR(10),
+    isDefault BOOLEAN,
+    inventory INT,
+    isInventory BOOLEAN,
+    level INT,
+    FOREIGN KEY (productSKU) REFERENCES products(sku)
+);
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    parentName VARCHAR(255),
+    slug VARCHAR(255),
+    level INT,
+    isActive BOOLEAN,
+    productSKU VARCHAR(255),
+    FOREIGN KEY (productSKU) REFERENCES products(sku)
+);
