db36395e5a044e15f6b527b85841cef8368a5051
minh phan <21115053120333@sv.ute.udn.vn>
2024-04-27 17:03:43 +0700

    update code data crawling

--- data_crawling/main.py
+++ data_crawling/main.py
@@ -9,11 +9,12 @@ from dotenv import load_dotenv
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-DATABASE_NAME=os.getenv("DB_NAME")
-HOST=os.getenv("DB_HOST")
-PORT=os.getenv("DB_PORT")
-USER=os.getenv("DB_USER")
-PASSWORD=os.getenv("DB_PASSWORD")
+DATABASE_NAME = os.getenv("DB_NAME")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+
 # Kết nối đến cơ sở dữ liệu MySQL
 db = mysql.connector.connect(
     host=HOST,
@@ -30,7 +31,8 @@ current_skip_count = 0
 
 @app.get("/")
 def index():
-    return {"title": "Hello:)"}
+    return {"title": "Hello :)"}
+
 @app.get("/save_products")
 async def save_products():
     global current_skip_count
@@ -47,58 +49,67 @@ async def save_products():
         for product in data["products"]:
             # Lưu thông tin sản phẩm vào bảng products
             insert_product_query = """
-                INSERT INTO products (sku, name, webName, image, slug, ingredients, dosageForm, brand, displayCode, isActive, isPublish, searchScoring, productRanking, specification)
+                INSERT INTO Products (sku, name, webName, image, slug, ingredients, dosageForm, brand, displayCode,
+                                      isActive, isPublish, searchScoring, productRanking, specification)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON DUPLICATE KEY UPDATE
-            name = VALUES(name),
-            webName = VALUES(webName),
-            image = VALUES(image),
-            slug = VALUES(slug),
-            ingredients = VALUES(ingredients),
-            dosageForm = VALUES(dosageForm),
-            brand = VALUES(brand),
-            displayCode = VALUES(displayCode),
-            isActive = VALUES(isActive),
-            isPublish = VALUES(isPublish),
-            searchScoring = VALUES(searchScoring),
-            productRanking = VALUES(productRanking),
-            specification = VALUES(specification)
+                    name = VALUES(name),
+                    webName = VALUES(webName),
+                    image = VALUES(image),
+                    slug = VALUES(slug),
+                    ingredients = VALUES(ingredients),
+                    dosageForm = VALUES(dosageForm),
+                    brand = VALUES(brand),
+                    displayCode = VALUES(displayCode),
+                    isActive = VALUES(isActive),
+                    isPublish = VALUES(isPublish),
+                    searchScoring = VALUES(searchScoring),
+                    productRanking = VALUES(productRanking),
+                    specification = VALUES(specification)
             """
             product_values = (
-                product["sku"], product["name"], product["webName"], product["image"], product["slug"], product["ingredients"],
-                product["dosageForm"], product["brand"], product["displayCode"], product["isActive"], product["isPublish"],
-                product["searchScoring"], product["productRanking"], product["specification"]
+                product["sku"], product["name"], product["webName"], product["image"], product["slug"],
+                product["ingredients"], product["dosageForm"], product["brand"], product["displayCode"],
+                product["isActive"], product["isPublish"], product["searchScoring"], product["productRanking"],
+                product["specification"]
             )
             cursor.execute(insert_product_query, product_values)
 
             # Lưu thông tin giá của sản phẩm vào bảng prices
             for price in product["prices"]:
                 insert_price_query = """
-                    INSERT INTO prices (id,productSKU, measureUnitCode, measureUnitName, isSellDefault, price, currencySymbol, isDefault, inventory, isInventory, level)
-                    VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    INSERT INTO Prices (id, productSKU, measureUnitCode, measureUnitName, isSellDefault, price,
+                                        currencySymbol, isDefault, inventory, isInventory, level)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     ON DUPLICATE KEY UPDATE
-                measureUnitCode = VALUES(measureUnitCode),
-                measureUnitName = VALUES(measureUnitName),
-                isSellDefault = VALUES(isSellDefault),
-                price = VALUES(price),
-                currencySymbol = VALUES(currencySymbol),
-                isDefault = VALUES(isDefault),
-                inventory = VALUES(inventory),
-                isInventory = VALUES(isInventory),
-                level = VALUES(level)
+                        measureUnitCode = VALUES(measureUnitCode),
+                        measureUnitName = VALUES(measureUnitName),
+                        isSellDefault = VALUES(isSellDefault),
+                        price = VALUES(price),
+                        currencySymbol = VALUES(currencySymbol),
+                        isDefault = VALUES(isDefault),
+                        inventory = VALUES(inventory),
+                        isInventory = VALUES(isInventory),
+                        level = VALUES(level)
                 """
                 price_values = (
-                    price["id"],product["sku"], price["measureUnitCode"], price["measureUnitName"], price["isSellDefault"], price["price"],
-                    price["currencySymbol"], price["isDefault"], price["inventory"], price["isInventory"], price["level"]
+                    price["id"], product["sku"], price["measureUnitCode"], price["measureUnitName"],
+                    price["isSellDefault"], price["price"], price["currencySymbol"], price["isDefault"],
+                    price["inventory"], price["isInventory"], price["level"]
                 )
                 cursor.execute(insert_price_query, price_values)
-            
+
             # Lưu thông tin danh mục của sản phẩm vào bảng categories
             for category in product["category"]:
                 insert_category_query = """
-                INSERT INTO categories (id, name, parentName, slug, level, isActive, productSKU)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ON DUPLICATE KEY UPDATE name=VALUES(name), parentName=VALUES(parentName), slug=VALUES(slug), level=VALUES(level), isActive=VALUES(isActive);
+                    INSERT INTO Categories (id, name, parentName, slug, level, isActive, productSKU)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
+                        name = VALUES(name),
+                        parentName = VALUES(parentName),
+                        slug = VALUES(slug),
+                        level = VALUES(level),
+                        isActive = VALUES(isActive)
                 """
                 category_values = (
                     category["id"], category["name"], category["parentName"], category["slug"], category["level"],
--- mysql/init_db/init.sql
+++ mysql/init_db/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE thuoclongchau;
 USE thuoclongchau;
 
-CREATE TABLE products (
+CREATE TABLE Products (
     sku VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255),
     webName VARCHAR(255),
@@ -18,7 +18,7 @@ CREATE TABLE products (
     specification VARCHAR(255)
 );
 
-CREATE TABLE prices (
+CREATE TABLE Prices (
     id INT PRIMARY KEY,
     productSKU VARCHAR(255),
     measureUnitCode INT,
@@ -30,10 +30,10 @@ CREATE TABLE prices (
     inventory INT,
     isInventory BOOLEAN,
     level INT,
-    FOREIGN KEY (productSKU) REFERENCES products(sku)
+    FOREIGN KEY (productSKU) REFERENCES Products(sku)
 );
 
-CREATE TABLE categories (
+CREATE TABLE Categories (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     parentName VARCHAR(255),
@@ -41,5 +41,32 @@ CREATE TABLE categories (
     level INT,
     isActive BOOLEAN,
     productSKU VARCHAR(255),
-    FOREIGN KEY (productSKU) REFERENCES products(sku)
+    FOREIGN KEY (productSKU) REFERENCES Products(sku)
+);
+
+CREATE TABLE Customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    passowrd VARCHAR(255) NULL,
+    is_verified tinyint(1),
+    phone VARCHAR(20),
+    address VARCHAR(255)
+);
+CREATE TABLE Orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATETIME NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(id)
+);
+CREATE TABLE Order_Details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_sku VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(id),
+    FOREIGN KEY (product_sku) REFERENCES Products(sku)
 );
