d377c8512db04c6cda8d975f5ac8532e1085ff6e
minh phan <21115053120333@sv.ute.udn.vn>
2024-04-30 17:57:23 +0700

    update format code crawling data

--- data_crawling/main.py
+++ data_crawling/main.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI
-import requests
-import mysql.connector
-from apscheduler.schedulers.background import BackgroundScheduler
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import mysql.connector
+from fastapi import FastAPI
+import requests
 
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
+# Environment variables
 DATABASE_NAME = os.getenv("DB_NAME")
 HOST = os.getenv("DB_HOST")
 PORT = os.getenv("DB_PORT")
 USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 
-# Kết nối đến cơ sở dữ liệu MySQL
+# Database connection
 db = mysql.connector.connect(
     host=HOST,
     user=USER,
@@ -27,7 +27,85 @@ cursor = db.cursor()
 
 app = FastAPI()
 
+# Global variables
 current_skip_count = 0
+API_URL = "https://api.nhathuoclongchau.com.vn/lccus/search-product-service/api/products/ecom/product/search/cate"
+MAX_RESULT_COUNT = 5
+
+# Helper functions
+def save_product(product):
+    insert_product_query = """
+        INSERT INTO Products (sku, name, webName, image, slug, ingredients, dosageForm, brand, displayCode,
+                              isActive, isPublish, searchScoring, productRanking, specification)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            name = VALUES(name),
+            webName = VALUES(webName),
+            image = VALUES(image),
+            slug = VALUES(slug),
+            ingredients = VALUES(ingredients),
+            dosageForm = VALUES(dosageForm),
+            brand = VALUES(brand),
+            displayCode = VALUES(displayCode),
+            isActive = VALUES(isActive),
+            isPublish = VALUES(isPublish),
+            searchScoring = VALUES(searchScoring),
+            productRanking = VALUES(productRanking),
+            specification = VALUES(specification)
+    """
+    product_values = (
+        product["sku"], product["name"], product["webName"], product["image"], product["slug"],
+        product["ingredients"], product["dosageForm"], product["brand"], product["displayCode"],
+        product["isActive"], product["isPublish"], product["searchScoring"], product["productRanking"],
+        product["specification"]
+    )
+    cursor.execute(insert_product_query, product_values)
+
+    for price in product["prices"]:
+        save_price(product["sku"], price)
+
+    for category in product["category"]:
+        save_category(product["sku"], category)
+
+def save_price(product_sku, price):
+    insert_price_query = """
+        INSERT INTO Prices (id, productSKU, measureUnitCode, measureUnitName, isSellDefault, price,
+                            currencySymbol, isDefault, inventory, isInventory, level)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            measureUnitCode = VALUES(measureUnitCode),
+            measureUnitName = VALUES(measureUnitName),
+            isSellDefault = VALUES(isSellDefault),
+            price = VALUES(price),
+            currencySymbol = VALUES(currencySymbol),
+            isDefault = VALUES(isDefault),
+            inventory = VALUES(inventory),
+            isInventory = VALUES(isInventory),
+            level = VALUES(level)
+    """
+    price_values = (
+        price["id"], product_sku, price["measureUnitCode"], price["measureUnitName"],
+        price["isSellDefault"], price["price"], price["currencySymbol"], price["isDefault"],
+        price["inventory"], price["isInventory"], price["level"]
+    )
+    cursor.execute(insert_price_query, price_values)
+
+def save_category(product_sku, category):
+    insert_category_query = """
+        INSERT INTO Categories (id, name, parentName, slug, level, isActive, productSKU)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            name = VALUES(name),
+            parentName = VALUES(parentName),
+            slug = VALUES(slug),
+            level = VALUES(level),
+            isActive = VALUES(isActive)
+    """
+    category_values = (
+        category["id"], category["name"], category["parentName"], category["slug"], category["level"],
+        category["isActive"], product_sku
+    )
+    cursor.execute(insert_category_query, category_values)
 
 @app.get("/")
 def index():
@@ -36,103 +114,30 @@ def index():
 @app.get("/save_products")
 def save_products():
     global current_skip_count
-    max_result_count = 5
-    url = "https://api.nhathuoclongchau.com.vn/lccus/search-product-service/api/products/ecom/product/search/cate"
-    body = {"skipCount": current_skip_count, "maxResultCount": max_result_count}
-    response = requests.post(url, json=body)
+    body = {"skipCount": current_skip_count, "maxResultCount": MAX_RESULT_COUNT}
+    response = requests.post(API_URL, json=body)
 
-    # Kiểm tra nếu yêu cầu thành công
     if response.status_code == 200:
         data = response.json()
 
-        # Xử lý dữ liệu và lưu vào cơ sở dữ liệu
         for product in data["products"]:
-            # Lưu thông tin sản phẩm vào bảng products
-            insert_product_query = """
-                INSERT INTO Products (sku, name, webName, image, slug, ingredients, dosageForm, brand, displayCode,
-                                      isActive, isPublish, searchScoring, productRanking, specification)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ON DUPLICATE KEY UPDATE
-                    name = VALUES(name),
-                    webName = VALUES(webName),
-                    image = VALUES(image),
-                    slug = VALUES(slug),
-                    ingredients = VALUES(ingredients),
-                    dosageForm = VALUES(dosageForm),
-                    brand = VALUES(brand),
-                    displayCode = VALUES(displayCode),
-                    isActive = VALUES(isActive),
-                    isPublish = VALUES(isPublish),
-                    searchScoring = VALUES(searchScoring),
-                    productRanking = VALUES(productRanking),
-                    specification = VALUES(specification)
-            """
-            product_values = (
-                product["sku"], product["name"], product["webName"], product["image"], product["slug"],
-                product["ingredients"], product["dosageForm"], product["brand"], product["displayCode"],
-                product["isActive"], product["isPublish"], product["searchScoring"], product["productRanking"],
-                product["specification"]
-            )
-            cursor.execute(insert_product_query, product_values)
+            save_product(product)
 
-            # Lưu thông tin giá của sản phẩm vào bảng prices
-            for price in product["prices"]:
-                insert_price_query = """
-                    INSERT INTO Prices (id, productSKU, measureUnitCode, measureUnitName, isSellDefault, price,
-                                        currencySymbol, isDefault, inventory, isInventory, level)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    ON DUPLICATE KEY UPDATE
-                        measureUnitCode = VALUES(measureUnitCode),
-                        measureUnitName = VALUES(measureUnitName),
-                        isSellDefault = VALUES(isSellDefault),
-                        price = VALUES(price),
-                        currencySymbol = VALUES(currencySymbol),
-                        isDefault = VALUES(isDefault),
-                        inventory = VALUES(inventory),
-                        isInventory = VALUES(isInventory),
-                        level = VALUES(level)
-                """
-                price_values = (
-                    price["id"], product["sku"], price["measureUnitCode"], price["measureUnitName"],
-                    price["isSellDefault"], price["price"], price["currencySymbol"], price["isDefault"],
-                    price["inventory"], price["isInventory"], price["level"]
-                )
-                cursor.execute(insert_price_query, price_values)
-
-            # Lưu thông tin danh mục của sản phẩm vào bảng categories
-            for category in product["category"]:
-                insert_category_query = """
-                    INSERT INTO Categories (id, name, parentName, slug, level, isActive, productSKU)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)
-                    ON DUPLICATE KEY UPDATE
-                        name = VALUES(name),
-                        parentName = VALUES(parentName),
-                        slug = VALUES(slug),
-                        level = VALUES(level),
-                        isActive = VALUES(isActive)
-                """
-                category_values = (
-                    category["id"], category["name"], category["parentName"], category["slug"], category["level"],
-                    category["isActive"], product["sku"]
-                )
-                cursor.execute(insert_category_query, category_values)
-
-        # Commit các thay đổi vào cơ sở dữ liệu
         db.commit()
-        current_skip_count += max_result_count
-        return {"message": "Dữ liệu đã được lưu thành công vào cơ sở dữ liệu"}
+        current_skip_count += MAX_RESULT_COUNT
+        return {"message": "Data saved successfully"}
 
-    # Trả về thông báo lỗi nếu yêu cầu không thành công
     else:
-        return {"error": f"Yêu cầu không thành công. Mã lỗi: {response.status_code}"}
-    
+        return {"error": f"Request failed with status code: {response.status_code}"}
+
 # Cấu hình lịch trình để crawl dữ liệu hằng ngày 
 # scheduler = BackgroundScheduler()
-# scheduler.add_job(save_products, 'cron', hour=0, minute=0)# Chạy vào lúc 00:00
-# scheduler.add_job(save_products, 'interval', seconds=10)# Chạy sau 10 giây sử dụng seconds, minutes, hours hoặc days.
+# scheduler.add_job(save_products, 'cron', hour=0, minute=0)  # Run at 00:00
+# scheduler.add_job(save_products, 'interval', seconds=10)  # Chạy sau 10 giây sử dụng seconds, minutes, hours hoặc days.
 # scheduler.start()
 
-# Dừng tác vụ lặp lại khi ứng dụng tắt
+# Stop background tasks on shutdown
 @app.on_event("shutdown")
 async def shutdown_event():
-    scheduler.shutdown()
+    # scheduler.shutdown()
+    db.close()
